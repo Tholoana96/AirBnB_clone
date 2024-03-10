663b2d1baf742de2281025d2af0198fb6effd930
Nomcebo1992 <129055067+Nomcebo1992@users.noreply.github.com>
2024-03-11 00:59:39 +0200

    console.py

--- console.py
+++ console.py
@@ -5,14 +5,10 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
-import models
-
-classes = {'BaseModel': BaseModel, 'User': User}
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
-
     def do_quit(self, arg):
         """Quit command to exit the commandline"""
         return True
@@ -29,10 +25,10 @@ class HBNBCommand(cmd.Cmd):
         """Creates a new instance of BaseModel"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
-            new = classes[arg]()
+            new = BaseModel()
             new.save()
             print(new.id)
 
@@ -40,58 +36,57 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representation of an instance based on the class name and id"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
             print("** instance id missing **")
-
+    pass
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
             print("** instance id missing **")
-
+    pass
     def do_all(self, arg):
         """Prints all string representation of all instances based or not on the class name"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
-            instances = models.storage.all()
-            print([str(instance) for instance in instances.values() if instance.__class__.__name__ == arg])
-
+            print("** instance id missing **")
+    pass
     def do_update(self, arg):
         """Updates an instance based on the class name and id by adding or updating attribute"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
             print("** instance id missing **")
-
+    pass
     def do_count(self, arg):
         """Retrieves the number of instances of a class"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
-            instances = models.storage.all()
-            count = sum(1 for instance in instances.values() if instance.__class__.__name__ == arg)
-            print(count)
+            print("** instance id missing **")
 
+    pass
     def do_BaseModel(self, arg):
         """Retrieves an instance based on its ID"""
         if not arg:
             print("** class name missing **")
-        elif arg not in classes:
+        elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
             print("** instance id missing **")
+    pass
 
 
 if __name__ == '__main__':
